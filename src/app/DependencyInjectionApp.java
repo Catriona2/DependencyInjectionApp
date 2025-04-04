fd0b13d9517b3d0ca1ea25fac97dbe3033a63fa8
Awor  Catriona <aworachuil@gmail.com>
2025-04-04 09:37:31 +0300

    commit three
    i have created the descriptive  variable names for  user  communication in the application specifically mailing and sms messaging

--- src/app/DependencyInjectionApp.java
+++ src/app/DependencyInjectionApp.java
@@ -10,15 +10,14 @@ public class DependencyInjectionApp {
     public static void main(String[] args) {
         Scanner  keyboardData=new Scanner(System.in);
         System.out.println("please Enter the Email message");
-        String UserName =keyboardData.nextLine();
+        String useremailMessage =keyboardData.nextLine();
         //  Injection point
         NotificationManager emailNotifier = new NotificationManager(new EmailService());
-        emailNotifier.notifyUser("Hello"+UserName);
-
+        emailNotifier.notifyUser("Hello"+useremailMessage);
         //  Another Injection  point
         NotificationManager smsNotifier = new NotificationManager(new SMSService());
         System.out.println("please Enter the sms message");
-        String UserName2 =keyboardData.nextLine();
-        smsNotifier.notifyUser("Hello,"+UserName2);
+        String userSMSmessage =keyboardData.nextLine();
+        smsNotifier.notifyUser("Hello,"+userSMSmessage);
     }
 }
